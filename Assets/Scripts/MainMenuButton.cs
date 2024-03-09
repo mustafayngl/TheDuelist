51520ebed437b1b122cda0640b37f342e511ad68
sadikgell <sadikgell@gmail.com>
2024-03-09 18:45:20 +0300

    MainMenuFinished

---
+++ Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.Animations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject optionsButton;
+    [SerializeField] private GameObject quitButton;
+    [SerializeField] private GameObject SoundOnOffButton;
+    [SerializeField] private GameObject soundOnOffText;
+    [SerializeField] private TextMeshProUGUI soundOnOffTMP;
+
+    [SerializeField] private GameObject optionsMenu;
+
+    void Start()
+    {
+        //soundOnOffTMP = soundOnOffText.GetComponent<TextMeshProUGUI>();
+    }
+    
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        playButton.SetActive(false);
+        quitButton.SetActive(false);
+        optionsButton.SetActive(false);
+    }
+
+    public void Options()
+    {
+        optionsMenu.SetActive(true);
+        playButton.SetActive(false);
+        quitButton.SetActive(false);
+        optionsButton.SetActive(false);
+
+        
+    }
+    public void ReturnMainMenu()
+    {
+        optionsMenu.SetActive(false);
+        playButton.SetActive(true);
+        quitButton.SetActive(true);
+        optionsButton.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Sound()
+    {
+        if (AudioListener.pause)
+        {
+            soundOnOffTMP.text = "Sound Off";
+            AudioListener.pause = false;
+        }
+        else
+        {
+            soundOnOffTMP.text = "Sound On";
+            AudioListener.pause = true;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
