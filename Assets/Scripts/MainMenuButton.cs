a2c5c8fd2612ae6a23a868ff76a93c7762beb885
sadikgell <sadikgell@gmail.com>
2024-03-10 18:21:07 +0300

    buildable version

--- Assets/Scripts/MainMenuButton.cs
+++ Assets/Scripts/MainMenuButton.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MainMenuButton : MonoBehaviour
 {
